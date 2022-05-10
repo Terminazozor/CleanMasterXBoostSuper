7c44887a3cbddda12a25fd9463c55a890a6641bc
Vangeyte Valentin <valentin.vangeyte@student.hers.be>
2022-05-10 15:31:05 +0200

    feat cleaning: start of cleaning computer
    add of 2 news methode to config and start the cleaning in new class methode Cleaning
    
    feat #1

---
+++ CleanMasterXBoostSuper/Cleaning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMasterXBoostSuper
+{
+    public class Cleaning
+    {
+        public void ConfigCleaning()
+        {
+            ProcessStartInfo infos = new ProcessStartInfo("cleanmgr.exe","sageset:1");
+            Process proc = Process.Start(infos);
+        }
+        public void LunchCleaning()
+        {
+            ProcessStartInfo infos = new ProcessStartInfo("cleanmgr.exe","sagerun:1");
+            Process proc = Process.Start(infos);
+        }
+    }
+}
